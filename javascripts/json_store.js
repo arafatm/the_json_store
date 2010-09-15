b84110cf0d02298d0349e57b96972d3d1e40f4c7
Arafat Mohamed <github@arafatm.com>
2010-09-14 20:50:36 -0500

    Added ajax code to return item detail

--- javascripts/json_store.js
+++ javascripts/json_store.js
@@ -26,7 +26,18 @@
     });
 
     this.get('#/item/:id', function(context) {
-      console.log(this.params['id']);
+      console.log("details for item " + context.params['id']);
+      $.ajax({
+        url: 'data/items.js', 
+        dataType: 'json',
+        success: function(items) {
+          context.items = items;
+          console.log("returned " + items.length + " items");
+          context.item = context.items[context.params['id']];
+          if (!context.item) { return context.notFound(); }
+          context.partial('templates/item_detail.template');
+        }
+      });
     });
 
 
